379edaf14abd4f3f2fbf9bfd969b1ea24927a907
auuruum <aurimasdovgialo@gmail.com>
2025-10-11 14:00:57 +0300

    Update admin_send_news.py

--- src/commands/admin/admin_send_news.py
+++ src/commands/admin/admin_send_news.py
@@ -31,7 +31,9 @@ class adminSendNews(commands.Cog):
         image: discord.Attachment = Option(discord.Attachment, "Image to send", required=True),
         send_image_before_or_after_news: str = Option(str, "Send image before or after news", choices=["before", "after"], default="before"),
         send_to_all_users: bool = Option(bool, "Send news to all users", default=True),
-        send_to_all_channels: bool = Option(bool, "Send news to all channels", default=True),
+        sent_to_all_users_with_role: discord.Role = Option(discord.Role, "Role to send news to", required=False, default=None),
+        send_to_all_channels: list[discord.TextChannel] = Option(discord.TextChannel, "Channels to send news to", required=False, default=None),
+        send_ghost_ping: bool = Option(description="Send ghost ping", default=False),
         schedule_time: str = Option(str, "Schedule time to send news (format: HH:MM)", required=False, default=None),
     ):
         await ctx.defer(ephemeral=True)
